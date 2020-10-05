9e9060f60a7633c89eab444bb41c4a87de92fcaf
tewtew023 <abcdtivolitwin@gmail.com>
2020-10-05 15:11:31 +0700

    Fix bug and real Finish

--- src/stringtoint/StringToInt.java
+++ src/stringtoint/StringToInt.java
@@ -8,62 +8,47 @@ public class StringToInt {
 		// TODO Auto-generated constructor stub
 	}
 
-	static int outPutInt;
-	static char strChar;
-
+	private static int intOutput;
+	static int output = 0;
 	public static void main(String[] args) {
+		
 
 		Scanner strSC = new Scanner(System.in);
 		String strInput;
 
 		System.out.print("Enter String : ");
 		strInput = strSC.nextLine();
-
-		int digit = digitCount(strInput);
-       System.out.print(toInt(digit,strInput));
+		detectInt(strInput);
+		System.out.print("Your Integer is : "+output);
 
 	}
 
-	public static int digitCount(String strInput) {
-
-		boolean isDigit;
-		int strLenght = strInput.length();
-		int digit = 0;
-		for (int i = 0; i < strLenght; i++) {
-			strChar = strInput.charAt(i);
-			if (Character.isDigit(strChar)) {
-				digit++;
+	static void detectInt(String strInput) {
+		char character;
+		char allNumber[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+		String onlyIntStr = "";
+		for ( int i=0;i<strInput.length();i++) {
+			character = strInput.charAt(i);
+			for(int ii=0;ii<10;ii++) {
+			if(character==allNumber[ii]) {
+				onlyIntStr+=character;	
 			}
 
-		}
-
-		return digit;
-
-	}
-
-	public static int toInt(int digit, String strInput) {
-		boolean isDigit;
-		int outPut=0;
-		int outPut2=0;
-		int strLenght = strInput.length();
-		for (int i = 0; i < strLenght; i++) {
-			strChar = strInput.charAt(i);
-			if (Character.isDigit(strChar)) {
-				outPut = (strChar - '0');
-				outPut=(int) (outPut*(Math.pow(10,digit-1)));
-			//	outPut=outPut*(10^digit);
-				System.out.println(outPut);
-				digit--;
-
-			 outPut2+=outPut;
 			}
 			
-
 		}
-		return outPut2;
-
+		toInteger(onlyIntStr);
 		
 
 	}
+	 static void toInteger(String onlyIntStr) {
+
+		 int digit=1;
+		 for(int i=onlyIntStr.length();i>0;i--) {
+			 output += (onlyIntStr.charAt(i-1)-'0')*digit;
+			 digit*=10;
+		 }
+	 }
 
 }
+
